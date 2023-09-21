3966f4242d0a4b745b272e100efa2648a1653003
samuel <samsala32@gmail.com>
2023-09-20 22:30:23 -0500

    correccion de tabla user

--- database/migrations/2023_09_21_032450_alter_users_table.php
+++ database/migrations/2023_09_21_032450_alter_users_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('clients', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email');
+        Schema::table('users', function(Blueprint $table){
             $table->string('telephone');
             $table->string('address');
-            $table->timestamps();
         });
     }
 
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('clients');
+       
     }
 };
