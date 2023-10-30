9094e730febd1ef5dc2f3e5010703ea659a1f09c
Samuel Salazar <samsala32@gmail.com>
2023-10-30 14:54:20 -0500

    Comment y Product arreglado

---
+++ app/Http/Controllers/Admin/AdminCommentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class AdminCommentController extends Controller
+{
+    public function index(){
+        $viewData = [];
+        $viewData["title"] = "Admin Page - Products - Online Store";
+        $viewData["products"] = Comment::all();
+        return view('admin.product.index')->with("viewData", $viewData);
+    }
+
+    public function store(Request $request){
+        $newComment=new Comment();
+        $newComment->setId($request->input('id'));
+        $newComment->setText($request->input('text'));
+        $newComment->setProductId($request->input('productId'));
+        $newComment->setProduct($request->input('product'));
+        $newComment->setUserId($request->input('userId'));
+        $newComment->setUser($request->input('user'));
+        $newComment->save();
+        return back();
+    }
+
+    public function update(Request $request, $id){
+        $comment=Comment::find($id);
+        if($comment){
+        $comment->setId($request->input('id'));
+        $comment->setText($request->input('text'));
+        $comment->setProductId($request->input('productId'));
+        $comment->setProduct($request->input('product'));
+        $comment->setUserId($request->input('userId'));
+        $comment->setUser($request->input('user'));
+        $comment->save();
+        return redirect()->route('admin.comment.index');
+        }
+        return back()->withErrors('comment not found');
+    }
+    
+    public function destroy($id){
+        $comment=Comment::find($id);
+        if($comment && $comment->getId()==$id){
+            $comment->delete();
+            return redirect()->route('admin.comment.index');
+        }
+        return redirect()->withErrors('comment not found');
+    }
+}
--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -30,8 +30,41 @@ class AdminProductController extends Controller
         $newProduct->setDescription($request->input('description'));
         $newProduct->setPrice($request->input('price'));
         $newProduct->setAvailability($request->input('availability'));
+        $imageFile = $request->file('image');
+        if ($imageFile && $imageFile->isValid()) {
+            $imageName = time() . '.' . $imageFile->getClientOriginalExtension();
+            $imagePath = $imageFile->storeAs('products', $imageName, 'public');
+            $newProduct->setImage($imagePath);
+        }
         $newProduct->save();
 
         return back();
+    }
+
+    public function update(Request $request, $id){
+        $product=Product::find($id);
+        if ($product) {
+            $request->validate([
+                "name" => "required|max:255",
+                "description" => "required",
+                "price" => "required|numeric|gt:0",
+                'image' => 'image',  // Asegúrate de manejar la subida de la imagen en el futuro
+            ]);
+        $product->setName($request->input('name'));
+        $product->setDescription($request->input('description'));
+        $product->setPrice($request->input('price'));
+        $product->save();
+        return redirect()->route('admin.product.index');
+        }
+        return back()->withErrors('Product not found.');
+    }
+
+    public function destroy($id){
+        $product=Product::find($id);
+        if($product && $product->getId()==$id){
+            $product->delete();
+            return redirect()->route('admin.product.index');
+        }
+        return redirect()->withErrors('product not found');
     }
 }
