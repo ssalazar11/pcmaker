c724fe923b28f23128ee4716e5a4c68922e814ac
Ortegago3 <Hortegag@eafit.edu.co>
2023-11-02 17:06:42 -0500

    Subo archivos y Front-End Plantilla, falta aplicarle el Back

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,3 +71,108 @@
     </div>
 </div>
 @endsection
+
+<!-- <!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Basic-icons.css">
+</head>
+
+<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="54">
+    <nav class="navbar navbar-expand-lg fixed-top bg-dark navbar-dark" id="mainNav">
+        <div class="container"><a class="navbar-brand" href="#page-top"><span style="color: rgb(229, 188, 49);">PCMaker</span></a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto text-uppercase">
+                    <li class="nav-item"><a class="nav-link" href="#services">login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">register</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">products</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">cart</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">home</a></li>
+                    <li class="nav-item"></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="masthead" style="background-image: url('assets/img/pc-gamer.webp');">
+        <div class="container">
+            <div class="intro-text" style="padding-top: 119px;padding-bottom: 31px;">
+                <section class="position-relative py-4 py-xl-5">
+                    <div class="container position-relative">
+                        <section class="position-relative py-4 py-xl-5">
+                            <div class="container">
+                                <div class="row d-flex justify-content-center">
+                                    <div class="col-md-6 col-xl-4">
+                                        <div class="card mb-5">
+                                            <div class="card-body d-flex flex-column align-items-center">
+                                                <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-person">
+                                                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
+                                                    </svg></div>
+                                                <form class="text-center" method="post">
+                                                    <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                                                    <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+                                                    <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Remember Me</label></div>
+                                                    <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div>
+                                                </form><a href="#">Forgot your password?</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </header>
+    <section id="contact" style="background-image:url('assets/img/map-image.png');">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">Contact Us</h2>
+                    <h3 class="text-muted section-subheading">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form id="contactForm" name="contactForm">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center"><span style="color: rgb(255, 255, 255);">hortegag@eafit.edu.co - Holmer Ortega - Developer</span></p>
+                            </div>
+                            <div class="col-md-6">
+                                <p class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center"><span style="color: rgb(255, 255, 255);">ssalazar1@eafit.edu.co - Samuel Salazar - Developer</span></p>
+                                <div class="form-group mb-3"><small class="form-text text-danger lead"></small></div>
+                            </div>
+                            <div class="w-100"></div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"><span class="copyright">Copyright&nbsp;© PCMaker 2023</span></div>
+                <div class="col-md-4">
+                    <ul class="list-inline quicklinks">
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a href="#">Terms of Use</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/agency.js"></script>
+</body>
+
+</html> -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,3 +69,133 @@
 </body>
 
 </html>
+
+<!-- <!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Basic-icons.css">
+</head>
+
+<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="54">
+    <nav class="navbar navbar-expand-lg fixed-top bg-dark navbar-dark" id="mainNav">
+        <div class="container"><a class="navbar-brand" href="#page-top"><span style="color: rgb(229, 188, 49);">PCMaker</span></a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto text-uppercase">
+                    <li class="nav-item"><a class="nav-link" href="#services">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#portfolio">Register</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">products</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">cart</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#about">home</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="masthead" style="background-image: url('assets/img/pc-gamer.webp');">
+        <div class="container">
+            <div class="intro-text" style="padding-top: 120px;padding-bottom: 25px;">
+                <div class="intro-lead-in"><span class="text-center"><strong><span style="color: rgb(229, 188, 49);">Welcome To PCMaker.</span></strong></span></div>
+                <div class="intro-heading text-uppercase"><span class="fs-1 text-center"><span style="color: rgb(229, 188, 49);">This is the first step to start in the world of technology!</span></span></div><a class="btn btn-primary btn-xl text-uppercase" role="button" href="#services" style="/*border-bottom-color: var(--bs-info-text-emphasis);*//*background-color: var(--bs-info-text-emphasis);*//*border-color: var(--bs-info-text-emphasis);*//*accent-color: var(--bs-info-text-emphasis);*/">take a look!</a>
+            </div>
+        </div>
+    </header>
+    <section id="services" style="padding-top: 14px;padding-bottom: 0px;margin: 0px;margin-bottom: -2px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">Services</h2>
+                    <h3 class="text-center text-muted section-subheading">Sales of laptops, desktop computers and computer components, all under the advice of professionals.</h3>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4 col-lg-12"><span class="fa-stack fa-4x"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i></span>
+                    <h3 class="text-center text-muted section-subheading"><span style="color: rgb(33, 37, 41);">If you want a powerful computer. PCMaker is your best choice.</span></h3>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="about" style="padding-top: 0px;padding-bottom: 0px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase">About</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p class="text-muted">We are dedicated to offering a specialized selection of technology products, including laptops, desktop computers, and various components such as graphics accelerators, RAM cards, and hard drives. Our main focus lies on customization, seeking to provide tailored technological solutions that satisfy both individual and corporate demands in the field of computing.<br><br>We address a wide spectrum of clients, from enthusiastic gamers to entrepreneurs and computer development professionals. To ensure we fully understand each client's needs, we conduct personal interviews, allowing us to gain insight into your requirements and provide accurate recommendations. We are committed to offering high quality products with guaranteed durability, ensuring the full satisfaction of our customers.<br><br>Our goal is to stand out as the most efficient online store in the field of technology, providing our customers with significant advantages compared to the current computer market. Unlike many other stores, we distinguish ourselves with our personalized approach and expert advice. We recognize that people often do not receive adequate guidance when purchasing a new computer, and it is precisely this differentiator that drives us to grow as a team and help our customers make informed and satisfactory decisions when choosing a new computer or components.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-light" id="team" style="padding: 0;">
+        <h1 class="text-center">OUR TEAM</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-lg-12">
+                    <div class="text-center text-sm-center text-lg-center text-xl-center text-xxl-center team-member"><img class="rounded-circle mx-auto" src="assets/img/team/Captura%20de%20pantalla%202023-10-30%20170948.png">
+                        <h4>Holmer Ortega.</h4>
+                        <p class="text-muted">Developer.</p>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-lg-12">
+                    <div class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center team-member"><img class="rounded-circle mx-auto" src="assets/img/team/Captura%20de%20pantalla%202023-10-30%20171011.png">
+                        <h4>Samuel Salazar.</h4>
+                        <p class="text-muted">Developer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-5" style="padding: 0px;"></section>
+    <section id="contact" style="background-image: url('assets/img/map-image.png');padding: 0px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">Contact Us</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form id="contactForm" name="contactForm">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <p class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center"><span style="color: rgb(255, 255, 255);">hortegag@eafit.edu.co - Holmer Ortega - Developer</span></p>
+                            </div>
+                            <div class="col-md-6">
+                                <p class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center"><span style="color: rgb(255, 255, 255);">ssalazar1@eafit.edu.co - Samuel Salazar - Developer</span></p>
+                                <div class="form-group mb-3"><small class="form-text text-danger lead"></small></div>
+                            </div>
+                            <div class="w-100"></div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer style="padding: 0px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"><span class="copyright">Copyright&nbsp;© PCMaker 2023</span></div>
+                <div class="col-md-4">
+                    <ul class="list-inline quicklinks">
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a href="#">Terms of Use</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/agency.js"></script>
+</body>
+
+</html> -->
