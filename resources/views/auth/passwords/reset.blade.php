77b0b2149a665d12e364aa4d43c6c9163b467322
Ortegago3 <Hortegag@eafit.edu.co>
2023-11-02 20:24:34 -0500

    Se implemneta Front funcional de layouts app y login

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+<!-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -62,7 +62,60 @@
         </div>
     </div>
 </div>
+@endsection -->
+
+@extends('layouts.app')
+
+@section('content')
+<header class="masthead" style="background-image: url('{{ asset('assets/img/pc-gamer.webp') }}');">
+    <div class="container">
+        <div class="intro-text" style="padding-top: 119px; padding-bottom: 31px;">
+            <section class="position-relative py-4 py-xl-5">
+                <div class="container position-relative">
+                    <section class="position-relative py-4 py-xl-5">
+                        <div class="container">
+                            <section class="position-relative py-4 py-xl-5">
+                                <div class="container">
+                                    <div class="row mb-5">
+                                        <div class="col-md-8 col-xl-6 text-center mx-auto">
+                                            <h2><span style="color: rgb(229, 188, 49);">Forgot your Password?</span></h2>
+                                        </div>
+                                    </div>
+                                    <div class="row d-flex justify-content-center">
+                                        <div class="col-md-6 col-xl-4">
+                                            <div class="card mb-5">
+                                                <div class="card-body d-flex flex-column align-items-center">
+                                                    <form class="text-center" method="post" action="{{ route('password.update') }}">
+                                                        @csrf
+                                                        <input type="hidden" name="token" value="{{ $token }}">
+
+                                                        <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus></div>
+
+                                                        <div class="mb-3"></div>
+
+                                                        <div class="mb-3"><input id="password" type="password" class="form-control" name="password" placeholder="Password" required autocomplete="new-password"></div>
+
+                                                        <div class="mb-3"></div>
+
+                                                        <div class="mb-3"><input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password"></div>
+
+                                                        <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Reset Password</button></div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </section>
+                </div>
+            </section>
+        </div>
+    </div>
+</header>
 @endsection
+
 
 <!-- <!DOCTYPE html>
 <html data-bs-theme="light" lang="en">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,52 +1,45 @@
-<!doctype html>
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ "PCMaker" }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script&amp;display=swap">
+    <link rel="stylesheet" href="{{ asset('/fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/Login-Form-Basic-icons.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 </head>
 
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ "PCMaker" }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="54">
+    <nav class="navbar navbar-expand-lg fixed-top bg-dark navbar-dark" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="#page-top">
+                <span style="color: rgb(229, 188, 49);">PCMaker</span>
+            </a>
+            <button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                </ul>
+                <ul class="navbar-nav ms-auto text-uppercase">
+                    <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+                    @guest
+                    <a class="nav-link active" href="{{ route('login') }}">Login</a>
+                    <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                    <a class="nav-link active" href="#about">About</a>
+                    <a class="nav-link active" href="{{ route('product.index') }}">Products</a> 
+                    <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a>
+                    <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        <div class="vr bg-white mx-2 d-none d-lg-block"></div>
-                        @guest
-                        <a class="nav-link active" href="{{ route('login') }}">Login</a>
-                        <a class="nav-link active" href="{{ route('register') }}">Register</a>
-                        <a class="nav-link active" href="{{ route('product.index') }}">Products</a> 
-                        <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a>
-                        <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
-                        
-                        @else
+                    @else
                         <a class="nav-link active" href="{{ route('product.index') }}">Products</a> 
                         <a class="nav-link active" href="{{ route('cart.index') }}">Cart</a>
                         <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
@@ -57,53 +50,23 @@
                             @csrf
                         </form>
                         @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
-
-</html>
-
-<!-- <!DOCTYPE html>
-<html data-bs-theme="light" lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Home - Brand</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700&amp;display=swap">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script&amp;display=swap">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Basic-icons.css">
-</head>
-
-<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="54">
-    <nav class="navbar navbar-expand-lg fixed-top bg-dark navbar-dark" id="mainNav">
-        <div class="container"><a class="navbar-brand" href="#page-top"><span style="color: rgb(229, 188, 49);">PCMaker</span></a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto text-uppercase">
-                    <li class="nav-item"><a class="nav-link" href="#services">Login</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#portfolio">Register</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#about">products</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#about">cart</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#about">home</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <header class="masthead" style="background-image: url('assets/img/pc-gamer.webp');">
+    <main class="py-4">
+            @yield('content')
+    </main>
+    <header class="masthead" style="background-image: url('{{ asset("/img/pc-gamer.webp") }}');">
         <div class="container">
             <div class="intro-text" style="padding-top: 120px;padding-bottom: 25px;">
-                <div class="intro-lead-in"><span class="text-center"><strong><span style="color: rgb(229, 188, 49);">Welcome To PCMaker.</span></strong></span></div>
-                <div class="intro-heading text-uppercase"><span class="fs-1 text-center"><span style="color: rgb(229, 188, 49);">This is the first step to start in the world of technology!</span></span></div><a class="btn btn-primary btn-xl text-uppercase" role="button" href="#services" style="/*border-bottom-color: var(--bs-info-text-emphasis);*//*background-color: var(--bs-info-text-emphasis);*//*border-color: var(--bs-info-text-emphasis);*//*accent-color: var(--bs-info-text-emphasis);*/">take a look!</a>
+                <div class="intro-lead-in">
+                    <span class="text-center"><strong><span style="color: rgb(229, 188, 49);">Welcome To PCMaker.</span></strong></span>
+                </div>
+                <div class="intro-heading text-uppercase">
+                    <span class="fs-1 text-center"><span style="color: rgb(229, 188, 49);">This is the first step to start in the world of technology!</span></span>
+                </div>
+                <a class="btn btn-primary btn-xl text-uppercase" role="button" href="#services" style="/*border-bottom-color: var(--bs-info-text-emphasis);*//*background-color: var(--bs-info-text-emphasis);*//*border-color: var(--bs-info-text-emphasis);*//*accent-color: var(--bs-info-text-emphasis);*/">take a look!</a>
             </div>
         </div>
     </header>
@@ -116,8 +79,14 @@
                 </div>
             </div>
             <div class="row text-center">
-                <div class="col-md-4 col-lg-12"><span class="fa-stack fa-4x"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i></span>
-                    <h3 class="text-center text-muted section-subheading"><span style="color: rgb(33, 37, 41);">If you want a powerful computer. PCMaker is your best choice.</span></h3>
+                <div class="col-md-4 col-lg-12">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h3 class="text-center text-muted section-subheading">
+                        <span style="color: rgb(33, 37, 41);">If you want a powerful computer. PCMaker is your best choice.</span>
+                    </h3>
                 </div>
             </div>
         </div>
@@ -131,7 +100,7 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <p class="text-muted">We are dedicated to offering a specialized selection of technology products, including laptops, desktop computers, and various components such as graphics accelerators, RAM cards, and hard drives. Our main focus lies on customization, seeking to provide tailored technological solutions that satisfy both individual and corporate demands in the field of computing.<br><br>We address a wide spectrum of clients, from enthusiastic gamers to entrepreneurs and computer development professionals. To ensure we fully understand each client's needs, we conduct personal interviews, allowing us to gain insight into your requirements and provide accurate recommendations. We are committed to offering high quality products with guaranteed durability, ensuring the full satisfaction of our customers.<br><br>Our goal is to stand out as the most efficient online store in the field of technology, providing our customers with significant advantages compared to the current computer market. Unlike many other stores, we distinguish ourselves with our personalized approach and expert advice. We recognize that people often do not receive adequate guidance when purchasing a new computer, and it is precisely this differentiator that drives us to grow as a team and help our customers make informed and satisfactory decisions when choosing a new computer or components.</p>
+                    <p class="text-muted">We are dedicated to offering a specialized selection of technology products, including laptops, desktop computers, and various components such as graphics accelerators, RAM cards, and hard drives. Our main focus lies on customization, seeking to provide tailored technological solutions that satisfy both individual and corporate demands in the field of computing.<br><br>We address a wide spectrum of clients, from enthusiastic gamers to entrepreneurs and computer development professionals. To ensure we fully understand each client's needs, we conduct personal interviews, allowing us to gain insight into your requirements and provide accurate recommendations. We are committed to offering high-quality products with guaranteed durability, ensuring the full satisfaction of our customers.<br><br>Our goal is to stand out as the most efficient online store in the field of technology, providing our customers with significant advantages compared to the current computer market. Unlike many other stores, we distinguish ourselves with our personalized approach and expert advice. We recognize that people often do not receive adequate guidance when purchasing a new computer, and it is precisely this differentiator that drives us to grow as a team and help our customers make informed and satisfactory decisions when choosing a new computer or components.</p>
                 </div>
             </div>
         </div>
@@ -141,13 +110,15 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-4 col-lg-12">
-                    <div class="text-center text-sm-center text-lg-center text-xl-center text-xxl-center team-member"><img class="rounded-circle mx-auto" src="assets/img/team/Captura%20de%20pantalla%202023-10-30%20170948.png">
+                    <div class="text-center text-sm-center text-lg-center text-xl-center text-xxl-center team-member">
+                        <img class="rounded-circle mx-auto" src="{{ asset('/img/team/Captura%20de%20pantalla%202023-10-30%20170948.png') }}">
                         <h4>Holmer Ortega.</h4>
                         <p class="text-muted">Developer.</p>
                     </div>
                 </div>
                 <div class="col-sm-4 col-lg-12">
-                    <div class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center team-member"><img class="rounded-circle mx-auto" src="assets/img/team/Captura%20de%20pantalla%202023-10-30%20171011.png">
+                    <div class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center team-member">
+                        <img class="rounded-circle mx-auto" src="{{ asset('/img/team/Captura%20de%20pantalla%202023-10-30%20171011.png') }}">
                         <h4>Samuel Salazar.</h4>
                         <p class="text-muted">Developer.</p>
                     </div>
@@ -156,7 +127,7 @@
         </div>
     </section>
     <section class="py-5" style="padding: 0px;"></section>
-    <section id="contact" style="background-image: url('assets/img/map-image.png');padding: 0px;">
+    <section id="contact" style="background-image: url('{{ asset('/img/map-image.png') }}');padding: 0px;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
@@ -168,10 +139,14 @@
                     <form id="contactForm" name="contactForm">
                         <div class="row">
                             <div class="col-md-6">
-                                <p class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center"><span style="color: rgb(255, 255, 255);">hortegag@eafit.edu.co - Holmer Ortega - Developer</span></p>
+                                <p class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center">
+                                    <span style="color: rgb(255, 255, 255);">hortegag@eafit.edu.co - Holmer Ortega - Developer</span>
+                                </p>
                             </div>
                             <div class="col-md-6">
-                                <p class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center"><span style="color: rgb(255, 255, 255);">ssalazar1@eafit.edu.co - Samuel Salazar - Developer</span></p>
+                                <p class="text-center text-sm-center text-md-center text-lg-center text-xl-center text-xxl-center">
+                                    <span style="color: rgb(255, 255, 255);">ssalazar1@eafit.edu.co - Samuel Salazar - Developer</span>
+                                </p>
                                 <div class="form-group mb-3"><small class="form-text text-danger lead"></small></div>
                             </div>
                             <div class="w-100"></div>
@@ -194,8 +169,8 @@
             </div>
         </div>
     </footer>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/js/agency.js"></script>
+    <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/js/agency.js') }}"></script>
 </body>
 
-</html> -->
+</html>
